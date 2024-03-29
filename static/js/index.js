3c0655370ae3bbeda41b12b9663160b657089939
Zenaof <113383146+Zenaof@users.noreply.github.com>
2024-03-29 15:11:00 +0300

    add 3 boxs and gif

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+var fire = document.getElementById('fire')
+
+var box1 = document.getElementById(boxs-9)
+var box2 = document.getElementById(boxs-11)
+
+var Cs = document.getElementById('Cs')
+// var Li = document.getElementById('Cs')
+
+var nowText = Cs
+
+function changeText()
+{
+   if (Cs.onclick)
+   {
+       box1.innerText = 'Цезий'
+   }
+}
