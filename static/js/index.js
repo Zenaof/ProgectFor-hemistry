b94e4627862203b145724f4f066dbc4702403017
Zenaof <113383146+Zenaof@users.noreply.github.com>
2024-03-30 10:33:48 +0300

    add change image, and a Li

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,15 @@
-var fire = document.getElementById('fire')
+let flame = document.getElementById('fire');
 
-var box1 = document.getElementById(boxs-9)
-var box2 = document.getElementById(boxs-11)
-
-var Cs = document.getElementById('Cs')
-// var Li = document.getElementById('Cs')
-
-var nowText = Cs
-
-function changeText()
+function changeCs()
 {
-   if (Cs.onclick)
-   {
-       box1.innerText = 'Цезий'
-   }
+    document.getElementById('bos-2').innerHTML = 'Цезий (Cs)';
+    document.getElementById('bos-6').innerHTML = 'Голубое окрашивание';
+    flame.src='../static/Images/blueFlame.gif';
+}
+
+function changeLi()
+{
+    document.getElementById('bos-2').innerHTML = 'Литий (Li)';
+    document.getElementById('bos-6').innerHTML = 'Красно-малиновое окрашивание';
+    flame.src='../static/Images/litiFlame.gif'
 }
